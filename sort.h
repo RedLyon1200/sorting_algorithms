4d564fdf7ff7d9302278f7603cd5bab24091576f
Crashed Code <s.agudelo1200@protonmail.com>
2020-06-12 20:42:56 +0000

    sort.h added

--- sort.h
+++ sort.h
@@ -1,5 +1,9 @@
+#ifndef SORT_H
+#define SORT_H
 #include <stdlib.h>
 #include <stdio.h>
+
+#include <unistd.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -10,11 +14,16 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
+
+void swap(listint_t *current, listint_t *move);
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+
+#endif
