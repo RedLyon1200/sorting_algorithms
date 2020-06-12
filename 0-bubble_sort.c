d2f190f3a3ed48aade6274ad49d106dd77b4472f
Crashed Code <s.agudelo1200@protonmail.com>
2020-06-12 16:30:21 +0000

    prototype of task 0 fixed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,32 @@
 #include "sort.h"
-
-
+#include <stdlib.h>
+#include <stdio.h>
 
 void bubble_sort(int *array, size_t size)
 {
-  int sorted = 1;
-  int i, j, tmp = 0;
+	int sorted = 1;
+	unsigned int j;
+	int tmp = 0;
+	unsigned int i;
 
-  i = size - 1;
+	i = size - 1;
 
-  while (sorted)
-  {
-	j = 0;
-	sorted = 0;
-	while (j < i)
+	while (sorted)
 	{
-	  if (array[j] > array[j + 1])
-	  {
-		sorted = 1;
-		tmp = array[j];
-		array[j] = array[j + 1];
-		array[j + 1] = tmp;
-		print_array(array, size);
-	  }
-	  j++;
+		j = 0;
+		sorted = 0;
+		while (j < i)
+		{
+			if (array[j] > array[j + 1])
+			{
+				sorted = 1;
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+			j++;
+		}
+		i--;
 	}
-	i--;
-  }
+}
