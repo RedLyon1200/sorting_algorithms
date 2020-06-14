99a3ecea92509b9f34e519d9c047c3851904e59c
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-14 17:32:12 +0000

    changed ordet to first contional

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current = (*list)->next, *aux = *list, *move = NULL;
 
-	if (*list == NULL || !list || (*list)->next == NULL)
+	if (!list || *list == NULL || (*list)->next == NULL)
 		return;
 
 	while (current)
