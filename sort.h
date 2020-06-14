f89009d0c47c58dc73e03e7953479c87b0acd81a
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-14 16:49:54 +0000

    add prototype functions for cocktail sorting

--- sort.h
+++ sort.h
@@ -23,6 +23,8 @@ void swap(listint_t *current, listint_t *move);
 void _swap(int *x, int *y);
 int split(int *array, int first, int last, size_t size);
 void sort_recursion(int *array, int first, int last, size_t size);
+void swap_cocktail(listint_t *first, listint_t *last, listint_t **list);
+listint_t *create_listint(const int *array, size_t size);
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
@@ -31,5 +33,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
