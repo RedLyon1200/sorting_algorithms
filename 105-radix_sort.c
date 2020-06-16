356128a0aa1e424f69044de9f8c08cb2459dbe84
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-16 16:35:56 +0000

    function that sorts an array of integers in ascending order using the Radix sort algorithm

---
+++ 105-radix_sort.c
@@ -0,0 +1,88 @@
+#include "sort.h"
+
+/**
+ * find_max_rad - get value max of array
+ * @array: pointer to list
+ * @size: size of array
+ * Return: max value
+ */
+
+int find_max_rad(int *array, size_t size)
+{
+	int max_value = 0;
+
+	while (size--)
+		if (array[size] > max_value)
+			max_value = array[size];
+	return (max_value);
+}
+
+/**
+ * rad_sort - radix sort
+ * @array: pointer to list
+ * @size: size of array
+ * @num: significant digit
+ * Return: None
+ */
+
+void rad_sort(int *array, size_t size, int num)
+{
+	int *tmp = NULL;
+	int i;
+	int *aux = NULL;
+
+	aux = malloc(size * sizeof(int));
+
+	tmp = malloc(size * sizeof(int));
+	if (tmp == NULL || aux == NULL)
+		return;
+
+	for (i = 0; i < (int)size; i++)
+		aux[i] = 0;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		aux[(array[i] / num) % 10]++;
+	}
+
+	for (i = 1; i < (int)size; i++)
+	{
+		aux[i] += aux[i - 1];
+	}
+
+	for (i = (int)size - 1; i >= 0; i--)
+	{
+		tmp[aux[(array[i] / num) % 10] - 1] = array[i];
+		aux[(array[i] / num) % 10]--;
+	}
+
+	for (i = 0; i < (int)size; i++)
+	{
+		array[i] = tmp[i];
+	}
+
+	free(tmp);
+}
+
+/**
+ * radix_sort - sort array with radix method
+ * @array: pointer to list
+ * @size: size of array
+ * Return: None
+ */
+
+void radix_sort(int *array, size_t size)
+{
+	int max, i;
+
+	if (!array || size <= 1)
+		return;
+
+	max = find_max_rad(array, size);
+
+	for (i = 1; max / i > 0; i *= 10)
+	{
+		rad_sort(array, size, i);
+		print_array(array, size);
+	}
+}
